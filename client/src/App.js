292d9e9e191b63e2123c337e9ebbc702e59be0b4
Souvik Kumar <souvik.cyc@gmail.com>
2024-11-09 18:21:40 +0530

    implement trigger backend API everytime someone visits vercel url

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from "./pages/Homepage";
@@ -5,6 +6,26 @@ import ResultPage from "./pages/ResultPage";
 import Navbar from "../src/components/Navbar";
 
 function App() {
+  useEffect(()=>{
+    const apiHealthEndpoint = process.env.REACT_APP_API_HEALTH_ENDPOINT || "http://localhost:8080/";
+
+    const triggerApi = async() => {
+      try{
+        const response = await fetch(apiHealthEndpoint, {
+          method: 'GET',
+        });
+        if (response.ok) {
+          console.log('API triggered successfully');
+        } else {
+          console.error('API trigger failed', response.statusText);
+        }
+      } catch (error) {
+        console.error("Error calling API:", error);
+      }
+    };
+    triggerApi();
+  }, []);
+  
   return (
     <div className="App">
       <Navbar/>
